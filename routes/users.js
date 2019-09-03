7fc8c1eed2c24adf89a6dacd602e115cfc516e60
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-09-03 23:12:24 +0900

    Add express validation of users

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const { check, validationResult } = require('express-validator');
+
 const User = require('../models/User');
 
 // @route      POST api/users
 // @desc       Register a user
 // @access     Public
-router.post('/', (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please enter your name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Enter a password with 6 or more characters').isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    res.send(req.body);
+  }
+);
 
 module.exports = router;
