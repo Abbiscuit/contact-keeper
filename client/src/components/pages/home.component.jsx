ebe6582bfb9db95498fc219b4d344d35f5a7daf0
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-09-04 16:05:51 +0900

    Create contact form & implement context dispatch action

--- client/src/components/pages/home.component.jsx
+++ client/src/components/pages/home.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Contact from '../contacts/contact.component';
+import ContactForm from '../contacts/contact-form.component';
 
 const Home = () => {
   return (
     <div className="grid-2">
-      <div>{/* ContactForm */}</div>
+      <div>
+        <ContactForm />
+      </div>
       <div>
         <Contact />
       </div>
