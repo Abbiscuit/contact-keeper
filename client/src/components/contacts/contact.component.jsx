f8fe2afaba791d276bc3e9e09aebe962b1caef00
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-09-04 17:34:16 +0900

    Add react-group-transition to the component

--- client/src/components/contacts/contact.component.jsx
+++ client/src/components/contacts/contact.component.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './contact-item.component';
 
 import ContactContext from '../../context/contact/contactContext';
@@ -14,13 +15,23 @@ const Contact = () => {
 
   return (
     <Fragment>
-      {filtered !== null
-        ? filtered.map(contact => {
-            return <ContactItem key={contact.id} contact={contact} />;
-          })
-        : contacts.map(contact => {
-            return <ContactItem key={contact.id} contact={contact} />;
-          })}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map(contact => {
+              return (
+                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              );
+            })
+          : contacts.map(contact => {
+              return (
+                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              );
+            })}
+      </TransitionGroup>
     </Fragment>
   );
 };
