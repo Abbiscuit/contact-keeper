9fb9be6c2a141dacc691586b8fab57c0a602e267
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-09-04 14:50:26 +0900

    Add contacts context

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,26 @@ import Navbar from './components/layout/navbar.component';
 import Home from './components/pages/home.component';
 import About from './components/pages/about.component';
 
+import ContactState from './context/contact/ContactState';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route exact path="/about" component={About}></Route>
-          </Switch>
-        </div>
-      </>
-    </BrowserRouter>
+    <ContactState>
+      <BrowserRouter>
+        <>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route exact path="/about" component={About}></Route>
+            </Switch>
+          </div>
+        </>
+      </BrowserRouter>
+    </ContactState>
   );
 };
 
