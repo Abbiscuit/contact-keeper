9bd4a1e986192213e720ca8c61969ebef692207c
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-09-04 17:53:15 +0900

    Create auth context & finish initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,29 @@ import Home from './components/pages/home.component';
 import About from './components/pages/about.component';
 
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <BrowserRouter>
-        <>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home}></Route>
-              <Route exact path="/about" component={About}></Route>
-            </Switch>
-          </div>
-        </>
-      </BrowserRouter>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <BrowserRouter>
+          <>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/about" component={About}></Route>
+              </Switch>
+            </div>
+          </>
+        </BrowserRouter>
+      </ContactState>
+    </AuthState>
   );
 };
 
